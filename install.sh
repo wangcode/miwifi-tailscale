d0ceee6b5ed5deed370a4f5469dab84da3de44b0
Wayne <wang@liujia.wang>
2024-08-27 09:19:24 +0000

    update script

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+version="1.72.1"
+install_path="/data/tailscale"
+bin_path="$install_path/bin"
+
+mkdir -p "$install_path/bin"
+curl -SL https://github.com/wangcode/miwifi-tailscale/releases/download/v$version/tailscale_v$version_arm64_upx.tar.gz | tar -zxC "$bin_path"
+ln -s "$bin_path/tailscaled" "$bin_path/tailscale"
 
 uci set firewall.tailscale=include
 uci set firewall.tailscale.type='script'
