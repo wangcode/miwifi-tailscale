07670e3d1f311bf609b5cb6d918adef45a186735
Wayne <wang@liujia.wang>
2024-09-16 23:35:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,5 +49,5 @@ uci set firewall.tailscale.path='/data/tailscale/start_up.sh'
 uci set firewall.tailscale.enabled='1'
 uci commit firewall
 
-chmod 755 /etc/init.d/tailscaled
-/etc/init.d/tailscaled start
+chmod +x "${install_path}/start_up.sh"
+sh "${install_path}/start_up.sh"
